c422c9b025e9b8317abbf88edd9d907a851af41f
Vlad <vladkochetov591@gmail.com>
2025-05-24 10:21:15 +0200

    tabliza umnozhenia 2.0

--- tablicia umnozhenia.cpp
+++ tablicia umnozhenia.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 
 int main(){
-	int multiplicand = 1;
-	int multiplier = 1;
-	for (int i = 1; i < 11; i++) {
-		multiplier = 1;
 
+	for (int i = 1; i < 11; i++) {
 		for (int j = 1; j < 11; j++) {
 
 			cout << i << "*" << j << "=" << i * j << " ";
